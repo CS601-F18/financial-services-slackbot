8c36dbcf109384c6162bd381f40717fafda13115
Nick Kebbas <nkebbas@usfca.edu>
2018-11-17 00:20:04 -0800

    get body from get request, send to db next

--- src/cs601/project4/slackbot/OauthConfirmStep2.java
+++ src/cs601/project4/slackbot/OauthConfirmStep2.java
@@ -1,13 +1,21 @@
 package cs601.project4.slackbot;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import java.util.stream.Collectors;
 
 import javax.net.ssl.HttpsURLConnection;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.input.*;
 
 /**
  * Oauth Authentication is a two part process. After the first part 
@@ -28,8 +36,21 @@ public class OauthConfirmStep2 extends HttpServlet {
         URL url = new URL("https://slack.com/api/oauth.access?code=" + request.getParameter("code") + "&client_id=" +Constants.CLIENT_ID + "&client_secret=" + Constants.CLIENT_SECRET + "&redirect_uri=" + Constants.REDIRECT);
         HttpsURLConnection connect = (HttpsURLConnection) url.openConnection();
         connect.getResponseCode();
+        InputStream in = connect.getInputStream();
+        String encoding = connect.getContentEncoding();
+        String body = IOUtils.toString(in, encoding);
+        System.out.println(body);
+        
+        /* Now get all that JSON Data because we need to save it */
+        /* TODO: Make this into a helper method because I keep using it */
+//        String getBody = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+//        System.out.println(getBody);
+//        JsonParser parser = new JsonParser();
+//        JsonObject jsonBody = (JsonObject) parser.parse(getBody);
+//        System.out.println(jsonBody);
+        
        // response.sendRedirect("https://slack.com/api/oauth.access?code=" + request.getParameter("code") + "&client_id=" +Constants.CLIENT_ID + "&client_secret=" + Constants.CLIENT_SECRET + "&redirect_uri=" + Constants.REDIRECT);
-        System.out.println("oauth confirm step 2 handler hit");
+       System.out.println("oauth confirm step 2 handler hit");
     }
     
     protected void doPost( HttpServletRequest request, 
